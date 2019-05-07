752de9f08b53dee18930d2568b1886f9a40eaf0f
Blaine <Ringospy@gmail.com>
2019-05-06 19:59:06 -0500

    Revert "Merge remote-tracking branch 'origin/master' into development"
    This reverts commit 55d2f67e213748136c059813447b55fc65cd58c6.

--- HelpDeskApplication/Admin/admin.php
+++ HelpDeskApplication/Admin/admin.php
@@ -1,7 +1,6 @@
 <?php include '../View/header.php'; ?>
 
 <div style="text-align: center">
-<<<<<<< HEAD
     <table class="table table-sm table-dark" id="AdminTable" style="width: 55%; margin: 0 auto; text-align: center">
         <h2>Current Admin Users</h2>
         <thead>
@@ -28,8 +27,6 @@
 <br>
 <hr>
 <div style="text-align: center">
-=======
->>>>>>> master
     <table class="table table-sm table-dark" id="UserTable" style="width: 55%; margin: 0 auto;">
         <h2>Current Users</h2>
         <thead>
@@ -37,7 +34,6 @@
                 <th scope="col">#</th>
                 <th scope="col">First</th>
                 <th scope="col">Last</th>
-<<<<<<< HEAD
                 <th scope="col">Role</th>
             </tr>
         </thead>
@@ -54,72 +50,28 @@
                     <button type="submit" class="btn btn-success" value="Query User Data">Query User Data</button>
                 </td>
             </tr>
-=======
-            </tr>
-        </thead>
-        <tbody>
-        <?php foreach ($users as $user) : ?>
-            <tr>
-                <th scope="row"><?php echo $user['userIndexId']; ?></th>
-                <td><?php echo $user['firstName']; ?></td>
-                <td><?php echo $user['lastName']; ?></td>
-                <td style="width: 15%">
-                    <form action="." method="post">
-                        <input type="hidden" name="action"
-                            value="Delete">
-                        <input type="hidden" name="user_id"
-                            value="<?php echo $user['userIndexId']; ?>">
-                        <input type="submit" value="Delete" class="btn btn-danger">
-                    </form>
-                </td>
-                <td style="width: 15%">
-                    <form action="." method="post">
-                        <input type="hidden" name="action"
-                            value="query">
-                        <input type="hidden" name="user_id"
-                            value="<?php echo $user['userIndexId']; ?>">
-                        <input type="submit" value="Query User Data" class="btn btn-success">
-                    </form>
-                </td>
-            </tr>
-            <?php endforeach; ?>
->>>>>>> master
         </tbody>
     </table>
 </div>
 <br>
 <hr>
-<<<<<<< HEAD
 <form>
-=======
-<form action="." method="post">
-    <input type="hidden" name="action" value="enter_user">
->>>>>>> master
     <div style="text-align: center;">
         <h2>Add Users</h2>
         <br>
         <div class="form-group">
             <label for="firstName">FirstName: </label>
             <input type="text" class="form-control" style="width: 10%;  display: inline" id="firstName"
-<<<<<<< HEAD
                 placeholder="FirstName">
-=======
-                placeholder="FirstName" name="fname">
->>>>>>> master
         </div>
         <div class="form-group">
             <label for="lastName">LastName: </label>
             <input type="text" class="form-control" style="width: 10%;  display: inline" id="lastName"
-<<<<<<< HEAD
                 placeholder="LastName">
-=======
-                placeholder="LastName" name="lname">
->>>>>>> master
         </div>
         <div class="form-group">
             <label for="userName">UserName: </label>
             <input type="text" class="form-control" style="width: 10%;  display: inline" id="userName"
-<<<<<<< HEAD
                 placeholder="UserName">
         </div>
         <div class="form-group">
@@ -130,11 +82,6 @@
             </select>
         </div>
         <button type="submit" class="btn btn-primary">Add User</button>
-=======
-                placeholder="UserName" name="uid">
-        </div>
-        <input type="submit" class="btn btn-primary" value="Add User">
->>>>>>> master
     </div>
 </form>
 <?php include '../View/footer.php'; ?>
