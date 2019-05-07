bae6f1315407d6c0762c62709aa6cc2a4cabd68c
IwccHelpDesk <48497882+IwccHelpDesk@users.noreply.github.com>
2019-05-06 19:13:47 -0500

    Add files via upload
    Jacob editions

--- HelpDeskApplication/Admin/index.php
+++ HelpDeskApplication/Admin/index.php
@@ -1,4 +1,44 @@
 <?php 
-        require_once '..Model/database.php';
-        require_once '..Model/admin_db.php';
+        require_once '../Model/database.php';
+        require_once '../Model/admin_db.php';
+
+        $action = filter_input(INPUT_POST, 'action');
+        if ($action == NULL) {
+                $action = filter_input(INPUT_GET, 'action');
+        }
+        if ($action == NULL) {
+                $action = 'index';
+        }
+
+        switch($action) {
+                case 'index':
+                        $users = get_users();
+                        include('admin.php');
+                        break;
+                case 'enter_user':
+                        $username = filter_input(INPUT_POST, 'uid');
+                        $lname = filter_input(INPUT_POST, 'lname');
+                        $fname = filter_input(INPUT_POST, 'fname');
+                        create_user($username, $lname, $fname);
+                        $users = get_users();
+                        include('admin.php');
+                        break;
+                case 'Delete':
+                        $id = filter_input(INPUT_POST, 'user_id');
+                        delete_user($id);
+                        $users = get_users();
+                        include('admin.php');
+                        break;
+                case 'query':
+                        $id = filter_input(INPUT_POST, 'user_id');
+                        $username = get_username($id);
+                        $user_info = reporting_info($id);
+                        include('query.php');
+                        break;
+                case 'home':
+                        $users = get_users();
+                        include('admin.php');
+                        break;
+
+        }
 ?>
--- HelpDeskApplication/Admin/verify_password.php
+++ HelpDeskApplication/Admin/verify_password.php
@@ -1,3 +1,15 @@
-<?php include '..View/header.php'; ?>
+<?php include '../View/header.php'; ?>
 
-<?php include '..View/footer.php'; ?>
+<form>
+    <div style="text-align: center; margin-top: 16%">
+        <h2>Administrator Verification</h2>
+        <div class="form-group">
+            <br>
+            <input type="password" class="form-control" style="width: 20%;  display: inline" id="AdminPassword"
+                placeholder="Enter Admin Password">
+        </div>
+        <button type="submit" class="btn btn-primary">Submit Admin Password</button>
+    </div>
+</form>
+
+<?php include '../View/footer.php'; ?>
