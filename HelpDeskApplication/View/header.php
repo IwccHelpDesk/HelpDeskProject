ea0b7b1fd3e5759e8c188ac3f4019af0e038dbc7
Blaine <Ringospy@gmail.com>
2019-05-06 20:18:48 -0500

    Revert "Merge branch 'development'"
    This reverts commit df25fb1f8156d6314b19e3f9623a65f3dd15395f.

--- HelpDeskApplication/View/header.php
+++ HelpDeskApplication/View/header.php
@@ -1,11 +1,10 @@
 <!DOCTYPE html>
     <html>
         <head>
-            <title>Help Desk</title>
+            <title>Login</title>
             <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" 
             integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" 
             crossorigin="anonymous">
-            <link rel="icon" href="https://iwcc.edu/_resources/images/iw2.ico">
         </head>
 
-    <body class="p-3 mb-2 bg-info text-white justify-content-center">
+    <body>
